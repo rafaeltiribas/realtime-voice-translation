70484a0e9a615a346a779e8fbf3bff0870149eeb
Rafael Tiribás <86385916+rafaeltiribas@users.noreply.github.com>
2023-11-08 17:19:50 -0300

    Update
    Consegui instalar o pyaudio. Falta agora inserir a API Cloud Speech-to-Text da Google. Instalar -> Autenticar -> Implementar.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # Real time speech to speech translator.
 # Speech to text -> Text translation -> Text to speech.
 # (Google Translate API) for [Text translation], intall using -pip install googletrans==3.1.0a0
+# (Google Cloud - Cloud Speech to Text) for [Speech to Text], install using -pip install --upgrade google-cloud-speech
 
 from googletrans import Translator
-
 import speech_recognition as sr
+import pyaudio
 
-import pyaudio # foi?
-
+'''
 init_rec = sr.Recognizer()
 print("Let's speak!!")
 with sr.Microphone() as source:
@@ -15,6 +15,7 @@ with sr.Microphone() as source:
     print("Recognizing your text.............")
     text = init_rec.recognize_google(audio_data)
     print(text)
+'''
 
 DEST_LANGUAGE = 'pt'
 
